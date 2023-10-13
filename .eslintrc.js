680701f7ffbf47f219b828131737caca3650d549
dhiraj chakma <chakmadhiraj@gmail.com>
2023-10-13 02:09:45 +0200

    ec

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  parser: 'babel-eslint',
+  extends: ['react-app'],
+  rules: {
+    'react/jsx-no-target-blank': 'warn', // Treat this as a warning, not an error
+    // Add other rules as needed
+  },
+};
