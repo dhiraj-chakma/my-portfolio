2c67215f779994be30e1ba839c75711e2f4fe5f8
dhiraj chakma <chakmadhiraj@gmail.com>
2023-10-13 02:16:41 +0200

    eclint2

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,7 @@
 module.exports = {
-  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: "module", // Specifies that your code uses ES Modules
+  },
   extends: ['react-app'],
   rules: {
     'react/jsx-no-target-blank': 'warn', // Treat this as a warning, not an error
